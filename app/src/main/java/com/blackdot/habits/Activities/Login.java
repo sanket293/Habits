b85a98792be3aff7a3a3778c408d0c998b884e50
sanku <scvagadiya@gmail.com>
2019-02-24 17:50:14 -0500

    added temporary habit list

--- app/src/main/java/com/blackdot/habits/Activities/Login.java
+++ app/src/main/java/com/blackdot/habits/Activities/Login.java
@@ -27,6 +27,8 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        
+
         findId();
 
     }
